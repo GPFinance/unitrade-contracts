967f8557f941d4d8a57e7dd37b4ede5fc1b15c0e
Marcelo Morgado <cmarcelom@gmail.com>
2020-12-30 18:30:27 +0000

    Removing unused Incinerator import

--- contracts/UniTradeMarketOrders.sol
+++ contracts/UniTradeMarketOrders.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
-import "./UniTradeIncinerator.sol";
 import "./IUniTradeStaker.sol";
 import "./UniTradeOrderBook.sol";
 
